5d538a2323fdb0e5bbc4b2cca9e2632c79c99bdb
nickrosendo <nicolasrosendo.dev@gmail.com>
2022-10-11 18:50:17 -0300

    feat: initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const theme = {
+    fg: "palevioletred",
+    bg: "white",
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
